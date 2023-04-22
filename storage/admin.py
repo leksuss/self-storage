29035d1ec1041ee165fb8523baa1d333f0b47e83
Andrey Alekseev <leksus@leksus.info>
2023-04-22 23:57:22 +0300

    add promocodes to admin panel

--- storage/admin.py
+++ storage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import User, Box, TransferRequest
+from .models import User, Box, TransferRequest, Promocodes
 
 admin.site.register(User)
 admin.site.register(Box)
 admin.site.register(TransferRequest)
+admin.site.register(Promocodes)
